f92a2970217b9d9cc823ee8043d19f716104402f
Rob Simpson <rsimpson2@me.com>
2025-08-30 10:06:10 -0400

    Time: 18 ms (58.72%) Space: 59.5 MB (53.07%)

---
+++ 0018-4sum/4sum.ts
@@ -0,0 +1,40 @@
+function fourSum(nums: number[], target: number): number[][] {
+    if (nums.length < 4) return [];
+
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    const result = [];
+
+    for (let i = 0; i < n - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        for (let j = i + 1; j < n - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+            let left = j + 1;
+            let right = n - 1;
+
+            while (left < right) {
+                let currentSum = nums[i] + nums[j] + nums[left] + nums[right];
+                if (currentSum === target) {
+                    result.push([nums[i], nums[j], nums[left], nums[right]]);
+
+                    while (left < right && nums[left] === nums[left + 1]) {
+                        left += 1;
+                    }
+                    while (left < right && nums[right] === nums[right - 1]) {
+                        right -= 1;
+                    }
+                    
+                    left += 1;
+                    right -= 1;
+                } else if (currentSum < target) {
+                    left += 1;
+                } else {
+                    right -= 1;
+                }
+            }
+        }
+    }
+    return result;
+};
