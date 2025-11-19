2d9c23fe79662b6ab54c8c7731dc774b343bf018
Rob Simpson <rsimpson2@me.com>
2025-11-19 09:17:35 -0500

    Time: 1 ms (29.90%) Space: 58.7 MB (27.24%)

---
+++ 0061-rotate-list/rotate-list.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function rotateRight(head: ListNode | null, k: number): ListNode | null {
+    if (!head || k === 0) {
+        return head;
+    }
+    let n = 1;
+    let tail = head;
+
+    while (tail.next !== null) {
+        tail = tail.next;
+        n++;
+    }
+
+    k = k % n;
+    if (k === 0) {
+        return head;
+    }
+
+    let fast: ListNode = head;
+    for (let i = 0; i < k; i++) {
+        fast = fast.next!;
+    }
+
+    let slow: ListNode = head;
+    while (fast.next !== null) {
+        fast = fast.next;
+        slow = slow.next!;
+    }
+
+    const newHead = slow.next;
+    fast.next = head;
+    slow.next = null;
+    return newHead;
+}
