a5c462903517fafc9b823a8c4e94878df6c3ed5d
Rob Simpson <rsimpson2@me.com>
2025-11-24 08:38:06 -0500

    Time: 2 ms (97.03%) Space: 59 MB (25.95%)

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.ts
@@ -0,0 +1,13 @@
+function smallerNumbersThanCurrent(nums: number[]): number[] {
+    const freq = new Array(101).fill(0);
+    for (const num of nums) {
+        freq[num]++;
+    }
+
+    const prefix = new Array(101).fill(0);
+    for (let i = 1; i <= 100; i++) {
+        prefix[i] = prefix[i - 1] + freq[i - 1];
+    }
+
+    return nums.map(num => prefix[num]);
+};
