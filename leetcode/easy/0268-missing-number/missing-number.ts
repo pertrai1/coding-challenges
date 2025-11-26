3ef78aa3534d65bb1d35c0263086e3d05389ddb4
Rob Simpson <rsimpson2@me.com>
2025-11-26 08:45:58 -0500

    Missing Number Challenge

--- leetcode/easy/0268-missing-number/missing-number.ts
+++ leetcode/easy/0268-missing-number/missing-number.ts
@@ -1,3 +1,12 @@
+/*
+@time: O(n) where n is the total numbers in nums
+@space: O(1) because the variables are storing constant numbers
+
+possible alternatives:
+Sorting: O(n log n) time
+Hash Set: O(n) time and O(n) space
+XOR trick: O(n) time and O(1) space
+*/
 export function missingNumber(nums: number[]): number {
   const n = nums.length;
   const expected = (n * (n + 1)) / 2;
