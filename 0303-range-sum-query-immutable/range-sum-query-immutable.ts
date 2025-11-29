fdfe1e5e868b0b849e2160848733bb24918fa258
Rob Simpson <rsimpson2@me.com>
2025-11-29 18:30:38 -0500

    Time: 11 ms (32.92%) Space: 64 MB (42.38%)

---
+++ 0303-range-sum-query-immutable/range-sum-query-immutable.ts
@@ -0,0 +1,18 @@
+class NumArray {
+    prefixSums: number[];
+
+    constructor(nums: number[]) {
+        this.prefixSums = new Array(nums.length + 1).fill(0);
+        for (let i = 0; i < nums.length; i++) {
+            this.prefixSums[i + 1] = nums[i] + this.prefixSums[i];
+        }
+    }
+
+    sumRange(left: number, right: number): number {
+        if (left === 0) {
+            return this.prefixSums[right + 1];
+        } else {
+            return this.prefixSums[right + 1] - this.prefixSums[left];
+        }
+    }
+}
