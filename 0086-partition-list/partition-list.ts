5df8c6c2be1a1e112e32569fcb5c0e7ffac654bb
Rob Simpson <rsimpson2@me.com>
2025-11-30 09:20:33 -0500

    Time: 0 ms (100.00%) Space: 58.7 MB (24.83%)

---
+++ 0086-partition-list/partition-list.ts
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/*
+Complexity
+@time: O(n) to traverse the list
+@space: O(1) the variables created are pointers to the list and the nodes themselves already exist in memory.
+*/
+function partition(head: ListNode | null, x: number): ListNode | null {
+  // create dummy head pointers
+  // these anchor to the start of each list
+  let lessDummy = new ListNode(0);
+  let geDummy = new ListNode(0);
+
+  // create dummy tail pointers
+  // move these to incrementally build the lists
+  let lessTail = lessDummy;
+  let geTail = geDummy;
+
+  // traverse the original list
+  let current = head;
+  while (current != null) {
+    if (current.val < x) {
+      lessTail.next = current;
+      lessTail = lessTail.next;
+    } else {
+      geTail.next = current;
+      geTail = geTail.next;
+    }
+    current = current.next;
+  }
+
+  // this will break the link with the original list
+  // terminate to avoid cycles
+  geTail.next = null;
+
+  // connect both of the lists
+  lessTail.next = geDummy.next;
+
+  // head of the less-than partition
+  return lessDummy.next;
+};
