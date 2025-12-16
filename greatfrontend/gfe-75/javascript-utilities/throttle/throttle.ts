af938737b675f6873272a1da12956ace1dc1d703
Rob Simpson <rsimpson2@me.com>
2025-12-16 09:24:59 -0500

    Update throttle leading edge and add trailing edge

--- greatfrontend/gfe-75/javascript-utilities/throttle/throttle.ts
+++ greatfrontend/gfe-75/javascript-utilities/throttle/throttle.ts
@@ -1,17 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 type ThrottleFn<T extends any[]> = (...args: T) => any;
 
+// Leading edge with timestamp
 export default function throttle<T extends any[]>(
   func: ThrottleFn<T>,
   wait: number
 ): ThrottleFn<T> {
-  let shouldBeThrottled = false;
+  let lastCallTime = 0;
+
   return function (this: any, ...args: T) {
-    if (!shouldBeThrottled) {
-      func.apply(this, args);
+    const now = Date.now();
+    if (now - lastCallTime >= wait) {
+      lastCallTime = now;
+      return func.apply(this, args);
     }
-    shouldBeThrottled = true;
-    setTimeout(() => {
-      shouldBeThrottled = false;
-    }, wait);
+  };
+}
+
+// Trailing edge
+export function throttleTrailingEdge<T extends any[]>(
+  func: ThrottleFn<T>,
+  wait: number
+): ThrottleFn<T> {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+  let lastArgs: T | undefined;
+
+  return function (this: any, ...args: T) {
+    lastArgs = args;
+    if (!timeoutId) {
+      timeoutId = setTimeout(() => {
+        if (lastArgs) {
+          func.apply(this, lastArgs);
+        }
+        timeoutId = null;
+      }, wait);
+    }
   };
 }
