cbef2af0a1d09fb71453343bf12c8982be2afe44
Rob Simpson <rsimpson2@me.com>
2026-01-05 09:02:46 -0500

    LeetCode Top K Frequent Words

--- leetcode/medium/0692-top-k-frequent-words/top-k-frequent-words.ts
+++ leetcode/medium/0692-top-k-frequent-words/top-k-frequent-words.ts
@@ -2,7 +2,7 @@
  * @time - O(n log n) where n is words
  * @space - O(n) n is each item in the Map
  */
-function topKFrequent(words: string[], k: number): string[] {
+export function topKFrequent(words: string[], k: number): string[] {
   const freqMap = new Map<string, number>();
   for (const word of words) {
     freqMap.set(word, (freqMap.get(word) || 0) + 1);
@@ -14,6 +14,6 @@ function topKFrequent(words: string[], k: number): string[] {
   for (let i = 0; i < k; i++) {
     result.push(sortedMap[i][0]);
   }
-  
+
   return result;
-};
+}
