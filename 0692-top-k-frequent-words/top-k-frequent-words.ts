4a0c2a5abff4600074dc0de0b8c7d00a87cf7ad0
Rob Simpson <rsimpson2@me.com>
2026-01-05 08:57:03 -0500

    Time: 9 ms (71.17%) Space: 61.2 MB (47.75%)

---
+++ 0692-top-k-frequent-words/top-k-frequent-words.ts
@@ -0,0 +1,19 @@
+/**
+ * @time - O(n log n) where n is words
+ * @space - O(n) n is each item in the Map
+ */
+function topKFrequent(words: string[], k: number): string[] {
+  const freqMap = new Map<string, number>();
+  for (const word of words) {
+    freqMap.set(word, (freqMap.get(word) || 0) + 1);
+  }
+
+  const sortedMap = Array.from(freqMap).sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]));
+  const result = [];
+
+  for (let i = 0; i < k; i++) {
+    result.push(sortedMap[i][0]);
+  }
+  
+  return result;
+};
