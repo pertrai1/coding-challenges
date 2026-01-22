3b147a27d9beb0cd539a66c9945f7beea2e342ac
rsimpson2 <rsimpson2@me.com>
2026-01-22 09:37:20 -0500

    LeetCode Rate Limiter Challenge

--- leetcode/easy/0359-logger-rate-limiter/logger-rate-limiter.ts
+++ leetcode/easy/0359-logger-rate-limiter/logger-rate-limiter.ts
@@ -1,9 +1,9 @@
-class Logger {
+export class Logger {
   messageMap = new Map<string, number>();
 
   shouldPrintMessage(timestamp: number, message: string): boolean {
     if (this.messageMap.has(message)) {
-      const lastTimestamp = this.messageMap.get(message);
+      const lastTimestamp = this.messageMap.get(message)!;
       if (timestamp >= lastTimestamp + 10) {
         this.messageMap.set(message, timestamp);
         return true;
