354761e6dd9bf9e9690dbf63bed10dbb94e797eb
Rob Simpson <rsimpson2@me.com>
2026-02-02 09:18:16 -0500

    Time: 14 ms (0.00%) Space: 58.8 MB (50.00%)

---
+++ 1084-find-k-length-substrings-with-no-repeated-characters/find-k-length-substrings-with-no-repeated-characters.ts
@@ -0,0 +1,44 @@
+/**
+  * @time - O(n) where n is the length of s
+  * @space - O(n) where n is the length of s
+ */
+function numKLenSubstrNoRepeats(s: string, k: number): number {
+  const n = s.length;
+
+  if (k > n) {
+    return 0;
+  }
+
+  let count = 0;
+  const window = new Map<string, number>();
+
+  // initialize the first window
+  for (let i = 0; i < k; i++) {
+    window.set(s[i], (window.get(s[i]) || 0) + 1);
+  }
+
+  if (window.size === k) {
+    count++;
+  }
+
+  // slide the window
+  for (let i = k; i < n; i++) {
+    // add a new character
+    window.set(s[i], (window.get(s[i]) || 0) + 1);
+
+    // remove olde character
+    const oldChar = s[i - k];
+    const oldCount = window.get(oldChar)!;
+    if (oldCount === 1) {
+      window.delete(oldChar);
+    } else {
+      window.set(oldChar, oldCount - 1);
+    }
+
+    if (window.size === k) {
+      count++;
+    }
+  }
+
+  return count; 
+};
