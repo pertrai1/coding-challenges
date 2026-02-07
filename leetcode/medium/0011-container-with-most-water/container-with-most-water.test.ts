dbf046a6e14866e9be2a8eb0dbfc8ecdc5d78ed2
rsimpson2 <rsimpson2@me.com>
2026-02-07 09:42:49 -0500

    test: cycle 2 (Green)

--- leetcode/medium/0011-container-with-most-water/container-with-most-water.test.ts
+++ leetcode/medium/0011-container-with-most-water/container-with-most-water.test.ts
@@ -5,4 +5,8 @@ describe('Container With Most Water', () => {
   it('should return 1 for height [1,1]', () => {
     expect(maxArea([1, 1])).toBe(1);
   });
+
+  it('should return 49 for height [1,8,6,2,5,4,8,3,7]', () => {
+    expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
+  });
 });
