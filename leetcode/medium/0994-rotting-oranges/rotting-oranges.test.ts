084bf92ef0ac1f3a3a9674017e181335531c2f11
rsimpson2 <rsimpson2@me.com>
2026-02-10 09:37:28 -0500

    learn: cycle 2 - test only rotten oranges (already passing)

--- leetcode/medium/0994-rotting-oranges/rotting-oranges.test.ts
+++ leetcode/medium/0994-rotting-oranges/rotting-oranges.test.ts
@@ -6,4 +6,9 @@ describe('Rotting Oranges', () => {
     const grid = [[0, 0]];
     expect(orangesRotting(grid)).toBe(0);
   });
+
+  it('should return 0 when there are only rotten oranges', () => {
+    const grid = [[2]];
+    expect(orangesRotting(grid)).toBe(0);
+  });
 });
