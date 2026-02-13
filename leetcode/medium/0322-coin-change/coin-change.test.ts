230674a58daa1f02afca31fc347c822feb9bd2d8
rsimpson2 <rsimpson2@me.com>
2026-02-13 09:00:32 -0500

    learn: cycle 3 - verify optimal solution with multiple coins (L2 assertion sketch)

--- leetcode/medium/0322-coin-change/coin-change.test.ts
+++ leetcode/medium/0322-coin-change/coin-change.test.ts
@@ -9,4 +9,8 @@ describe('Coin Change', () => {
   it('should return -1 when amount cannot be made with available coins', () => {
     expect(coinChange([2], 3)).toBe(-1);
   });
+
+  it('should return the minimum number of coins needed for a valid amount', () => {
+    expect(coinChange([1, 2, 5], 11)).toBe(3);
+  });
 });
